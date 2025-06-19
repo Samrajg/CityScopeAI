686af399516e7f0bdf66b15dd89e6c53bb93e22e
Godwin Samraj G <samrajgodwin7@gmail.com>
2025-06-19 15:04:56 +0530

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -12,6 +12,7 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier  # or whatever model you're using
 
